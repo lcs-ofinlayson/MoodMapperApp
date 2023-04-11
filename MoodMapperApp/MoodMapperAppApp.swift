9ab9c3208df987222636d6a2b3948219de60a114
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-04-11 09:03:43 -0400

    Initial Commit

---
+++ MoodMapperApp/MoodMapperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapperAppApp.swift
+//  MoodMapperApp
+//
+//  Created by Oliver Finlayson on 2023-04-11.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
